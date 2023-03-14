1230846b5d849f29e61fd745555ed12819749987
HNU <HNU@DESKTOP-8C4Q3SL>
2023-03-14 13:42:38 +0900

    update Q1

--- 230314-1/Program.cs
+++ 230314-1/Program.cs
@@ -6,7 +6,22 @@ namespace _230314_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("숫자 입력: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0)
+            {
+                Console.WriteLine("0입니다!");
+            }
+            else if (input > 0)
+            {
+                Console.WriteLine("양수 입니다!");
+            }
+            else
+            {
+                Console.WriteLine("음수 입니다!");
+            }
         }
     }
+}
 }
