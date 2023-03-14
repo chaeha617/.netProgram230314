2014ac93d8e8bb59fe12ab2515313ce0aa4b9e54
HNU <HNU@DESKTOP-8C4Q3SL>
2023-03-14 14:01:26 +0900

    update Q3

--- 230314-1/Program.cs
+++ 230314-1/Program.cs
@@ -6,7 +6,17 @@ namespace _230314_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 0;
+            Console.WriteLine("단어를 입력해주세요.");
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a' || input[i] == 'e' || input[i] == 'o'|| input[i] == 'i' || input[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count + "개의 모음이 있습니다.");
         }
     }
 }
