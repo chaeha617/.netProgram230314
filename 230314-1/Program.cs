1a30a1418acaef438e92b5e81482ddf8fa92b37d
HNU <HNU@DESKTOP-8C4Q3SL>
2023-03-14 14:52:40 +0900

    update Q4

--- 230314-1/Program.cs
+++ 230314-1/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
+using System.Linq;
 namespace _230314
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 0;
+            string answer = "";
+            Console.WriteLine("문자열을 입력해주세요.");
+            string input = Console.ReadLine();
+            string[] charArr = input.Split(' ');
+            int[] word = new int[input.Length];
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                word[i] = charArr.Count(f => (charArr[i] == f));
+                
+            }
+            int maxInd = Array.IndexOf(word, word.Max());
+            Console.WriteLine("word : " + charArr[maxInd]+", time :" + word[maxInd]);
         }
     }
 }
