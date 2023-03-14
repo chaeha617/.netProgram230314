ff32cfc9757a8a18d17270b4b42f2128f6eae70b
HNU <HNU@DESKTOP-8C4Q3SL>
2023-03-14 14:25:25 +0900

    update Q3

--- 230314-1/Program.cs
+++ 230314-1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _230314
 {
@@ -6,7 +7,20 @@ namespace _230314
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 0;
+            string answer = "";
+            Console.WriteLine("숫자를 입력해주세요.");
+            string input = Console.ReadLine();
+            char[] charArr = input.ToCharArray();
+            
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                if(charArr.Count(f => (charArr[i] == f)) == 1)
+                {
+                    answer += charArr[i];
+                }
+            }
+            Console.WriteLine(answer);
         }
     }
 }
