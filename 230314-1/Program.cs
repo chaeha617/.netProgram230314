bffcaa254104aa51df8cf3bf56fbfdd4823e523e
chaeha617 <rlacogk6577@daum.net>
2023-03-20 03:02:20 +0900

    update Q5,6,7

--- 230314-1/Program.cs
+++ 230314-1/Program.cs
@@ -6,7 +6,12 @@ namespace _230314
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("10진수 숫자를 입력하세요: ");
+            int number = int.Parse(Console.ReadLine());
+
+            string binaryString = Convert.ToString(number, 2);
+
+            Console.WriteLine("{0}의 이진수 표현: {1}", number, binaryString);
         }
     }
 }
