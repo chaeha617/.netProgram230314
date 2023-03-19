dedb4ef335ebdddfc2aeb7a66cdaf5061e929e0d
chaeha617 <rlacogk6577@daum.net>
2023-03-20 03:04:44 +0900

    Q5

--- 230314-1/Program.cs
+++ 230314-1/Program.cs
@@ -6,7 +6,18 @@ namespace _230314
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("숫자 개수 입력: ");
+            int input = int.Parse(Console.ReadLine());
+            int[] arr = new int[input];
+            for (int i = 0; i < input; i++)
+            {
+                Console.Write("숫자 입력: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            string numbersString = string.Join(" ", arr);
+            Console.WriteLine(numbersString);
         }
     }
 }
