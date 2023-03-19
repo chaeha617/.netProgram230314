351abd3f35ee41d6c69ec4826f5cfc1290ee68ed
chaeha617 <rlacogk6577@daum.net>
2023-03-20 03:03:53 +0900

    Q6

--- 230314-1/Program.cs
+++ 230314-1/Program.cs
@@ -6,7 +6,23 @@ namespace _230314
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("문자열을 입력하세요: ");
+            string inputString = Console.ReadLine();
+
+            string resultString = "";
+            foreach (char c in inputString)
+            {
+                if (Char.IsDigit(c))
+                {
+                    resultString += c;
+                }
+                else
+                {
+                    resultString += Char.ToUpper(c);
+                }
+            }
+
+            Console.WriteLine("결과: " + resultString);
         }
     }
 }
